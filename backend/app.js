570ad8a83df28c2f814c8f3469ebad72fef32972
Suchet <suchetv.p@gmail.com>
2024-06-17 16:40:06 -0400

    first commit

---
+++ backend/app.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import router from './routes/user-routes.js';
+import blogRouter from './routes/blog-routes.js';
+
+const app = express();
+app.use(express.json());
+app.use("/api/user",router);
+app.use("/api/blog", blogRouter);
+
+mongoose.connect('mongodb+srv://admin:ADsuch123@cluster0.sbzhovf.mongodb.net/Blog?retryWrites=true&w=majority&appName=Cluster0')
+        .then(() => app.listen(3000))
+        .then(() => console.log("Connected to database and listening to localhost 3000"))
+        .catch((err) => console.log(err));
+
+//ADsuch123
