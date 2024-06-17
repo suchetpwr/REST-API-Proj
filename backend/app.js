e77e048e3e6eedc07ff29a0c204afc553ee099e4
Suchet <suchetv.p@gmail.com>
2024-06-17 16:44:06 -0400

    Removing credentials from app.js

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use("/api/user",router);
 app.use("/api/blog", blogRouter);
 
-mongoose.connect('mongodb+srv://admin:ADsuch123@cluster0.sbzhovf.mongodb.net/Blog?retryWrites=true&w=majority&appName=Cluster0')
+mongoose.connect('mongodb+srv://<userID>:<Password>@cluster0.sbzhovf.mongodb.net/Blog?retryWrites=true&w=majority&appName=Cluster0')
         .then(() => app.listen(3000))
         .then(() => console.log("Connected to database and listening to localhost 3000"))
         .catch((err) => console.log(err));
